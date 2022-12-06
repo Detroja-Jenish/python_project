a55b2fc77f5df123ae069d5014f253c9283614b9
Detroja-Jenish <jenishdetroja2004@gmail.com>
2022-12-06 21:10:49 +0530

    login-signup set in webapp and now turn of making users directories system for data storage...

--- flask_project/bill/app.py
+++ flask_project/bill/app.py
@@ -3,14 +3,67 @@
 from flask import Flask, render_template, redirect, request
 import json
 import math
+from os import mkdir
 
 app = Flask("__name__")
 
 all_bill = []
 this_bill = []
+users = {} 
 
-@app.route("/index")
+@app.route("/")
 def index():
+    return render_template("index.html")
+@app.route("/signup-form")
+def signup_form():
+    return render_template("signup.html")
+
+@app.route("/login-form")
+def login_form():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    with open("./database/users.json", 'r') as f:
+        users = json.load(f);
+    
+    name = request.form.get("user-name")
+    password = request.form.get("password")
+
+    if name in users and password == users[name]:
+        return redirect("/bill")
+    else:
+        print(name)
+
+        return redirect("/login-fom")
+
+@app.route("/signup",methods=["POST"])
+def signup():
+    name = request.form.get("user-name")
+    print(name)
+    password = request.form.get("password")
+    confirm_password = request.form.get("confirm-password")
+
+    if password == confirm_password:
+
+        with open("./database/users.json", 'r') as f:
+            users = json.load(f);
+
+        users[name] = password;
+        print(users)
+
+        with open("./database/users.json", 'w') as f:
+            json.dump(users,f)
+        mkdir("./database/"+name)
+        return redirect("/bill?user=" + name)
+
+    else:
+        return redirect("/signup-form")
+    
+        
+
+@app.route("/bill")
+def bill_form():
     return render_template("bill.html")
 
 @app.route("/create_bill", methods=["GET", "POST"])
